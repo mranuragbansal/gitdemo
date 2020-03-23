feaaddf986a38fd6e6d8241964b77ebf9349a486
mranuragbansal <anurag2117@gmail.com>
2020-03-23 18:48:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
 print("hello")
 password = "hello123"
-key="dshndksjhndi2w829yu2e92uihjnkwnwknwnkwndkw"
-sas_token="password12345!@#"
-Authorization = 'rWJTjTMuAcU3VyWohCAvmIKEPqwANv47LTQfv9Bys9WLMdL6KaLmj8qsisZffFWtb'
 print("password")
